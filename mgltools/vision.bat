dc4e80af1e4afbd018c942ba31c5b2c9724fe7fd
MangalamGautamSinha <mangalamgsinha@gmail.com>
2025-08-22 01:38:48 +0530

    first commit

---
+++ mgltools/vision.bat
@@ -0,0 +1,3 @@
+set BABEL_DATADIR=C:\Users\MANGAL\Desktop\DockSuiteX\docksuitex\bin\mgltools\OpenBabel-2.3.2\data
+set path=C:\Users\MANGAL\Desktop\DockSuiteX\docksuitex\bin\mgltools\OpenBabel-2.3.2;%path%
+"C:\Users\MANGAL\Desktop\DockSuiteX\docksuitex\bin\mgltools\python.exe" "C:\Users\MANGAL\Desktop\DockSuiteX\docksuitex\bin\mgltools\Lib\site-packages\Vision\bin\runVision.py" %1 %2 %3 %4 %5 %6 %7 %8 %9 
